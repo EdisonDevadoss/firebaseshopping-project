e50367471a173a7d4f7c2d3fc8872a4c311738fa
Edison Devadoss <edisonj1996@gmail.com>
2017-10-23 11:55:47 +0530

    Firebase-shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { HttpModule } from '@angular/http';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEGXqmYyJNf1Cde4FzQdJ3yrRDg-Z_nO4",
+  authDomain: "groceries-194d2.firebaseapp.com",
+  databaseURL: "https://groceries-194d2.firebaseio.com",
+  projectId: "groceries-194d2",
+  storageBucket: "groceries-194d2.appspot.com",
+  messagingSenderId: "421367646677"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +30,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +43,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingItems: FirebaseListObservable<any[]>;
+  newItem = '';
 
+  constructor(public navCtrl: NavController, private firebaseService: FirebaseServiceProvider) {
+    this.shoppingItems = this.firebaseService.getShoppingItems();
+  }
+
+  addItem(){
+    this.firebaseService.addItem(this.newItem);
+  }
+  removeItem(id){
+    this.firebaseService.removeItem(id);
   }
 
 }
